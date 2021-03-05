78be8c9a19a61501229516c2275333c20de45701
Jordan <jbluehdorn@alphabytes.io>
2021-03-05 08:49:01 -0800

    Updated build tools

--- kahaiz_wheel/webpack.mix.js
+++ kahaiz_wheel/webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.react('src/js/app.js', 'dist/').sass('src/styles/app.scss', 'dist/');
+mix.js('src/js/app.js', 'dist/').react().sass('src/styles/app.scss', 'dist/');
 
 // Full API
 // mix.js(src, output);
