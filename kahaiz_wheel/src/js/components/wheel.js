809a1e1ce2e360a2079d0c762dc10020d34c57db
Jordan <jbluehdorn@alphabytes.io>
2021-10-04 08:41:58 -0700

    Spookified wheel

--- kahaiz_wheel/src/js/components/wheel.js
+++ kahaiz_wheel/src/js/components/wheel.js
@@ -150,7 +150,7 @@ export default class Wheel extends React.Component {
                     <div className="col-12">
                         <div className="card">
                             <div className="card-header">
-                                <h1 className="card-title text-center">The King's Wheel</h1>
+                                <h1 className="card-title text-center">💀 The King's Wheel of Spook 💀</h1>
                             </div>
                             <div className="card-body">
                                 <canvas ref={this.canvasRef} id="board" height="400" width="700"></canvas>
