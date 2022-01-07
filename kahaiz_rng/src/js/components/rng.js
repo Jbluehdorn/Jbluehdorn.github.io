b1a604c511ebbce8e9fb38f573f96d51a0e54b23
Jordan <jbluehdorn@alphabytes.io>
2022-01-07 09:21:41 -0800

    Reverted theme to normal

--- kahaiz_rng/src/js/components/rng.js
+++ kahaiz_rng/src/js/components/rng.js
@@ -119,7 +119,7 @@ export default class Rng extends React.Component {
                     <div className="col-12">
                         <div className="card">
                             <div className="card-header">
-                                <h1 className="card-title text-center">🎄 Merry Dice of Destiny 🎄</h1>
+                                <h1 className="card-title text-center">Dice of Destiny</h1>
                             </div>
                             <div className="card-body">
                                 { !!this.state.value &&
